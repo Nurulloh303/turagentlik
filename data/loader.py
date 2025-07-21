c2464d82c68d8ef7f5fc25843e33a0681b63a435
Nurulloh <nurulloh637@gmail.com>
2025-07-21 15:59:28 +0500

    1-kun tugadi

--- data/loader.py
+++ data/loader.py
@@ -1,4 +1,6 @@
 from telebot import TeleBot
 from config import TOKEN
+from database.database import Database
 
 bot = TeleBot(TOKEN)
+db = Database()
--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -1,5 +1,9 @@
+from telegram._utils import markup
+
 from data.loader import bot
 from telebot.types import Message
+
+from keyboards.inline import lang_button
 
 @bot.message_handler(commands=["start"])
 def start(message: Message):
@@ -8,4 +12,4 @@ def start(message: Message):
             f"Iltimos tilni tanglang!!!\n\n"
             f"🇬🇧Hello, welcome to FN30 tour agency!!!\nPlease select the language!!!\n\n"
             f"🇷🇺Здравствуйте, добро пожаловать в туристическое агентство FN30!!!\nПожалуйста, выберите язык!!!")
-    bot.send_message(chat_id, text)
+    bot.send_message(chat_id, text, reply_markup=lang_button())
